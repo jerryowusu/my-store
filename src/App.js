ba5a781442a9b3ed74482e10b750f5ba9adf0706
Jerry Owusu <jerryowusu6133@gmail.com>
2022-12-22 00:49:12 +0000

    Create the files and upgrade to state and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './Form';
+import Table from './Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        characters: []       
+    };
 
-export default App;
+    removeCharacter = (index) => {
+        const { characters} = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        })
+    }
+
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character] })
+    }
+
+    render() {
+        const { characters } = this.state;
+
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
+        </div>
+      )
+    }
+  }
+
+  export default App;
